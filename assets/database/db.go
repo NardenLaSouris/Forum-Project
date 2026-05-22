909f3f4f7f4b4e2fa703998d9ce9f4048e0abd0f
pomaxime <maxime.poyet@ynov.com>
2026-05-22 11:19:07 +0200

    [ADD] (assets/database...): ajout database)

---
+++ assets/database/db.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() error {
+    var err error
+
+    DB, err = sql.Open("sqlite3", "./forum.db")
+    if err != nil {
+        return err
+    }
+
+    return DB.Ping()
+}
